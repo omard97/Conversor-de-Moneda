32ae08703ee7ebf084e0b8f48b63a2b4decbc330
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-10-23 19:15:24 -0300

    conversión a bitcoins

--- script.js
+++ script.js
@@ -28,13 +28,16 @@ function conversion() {
     let resultado = 0;
     monto = document.getElementById('cantidad').value;
 
-    debugger
+    let bitcoin = 0;
+    let resultadoBitcoin = 0;
+
+  
     let posicion1 = document.getElementById('moneda1');
     let moneda1 = posicion1.options[posicion1.selectedIndex].value;
 
     let posicion2 = document.getElementById('moneda2');
     let moneda2 = posicion2.options[posicion2.selectedIndex].value;
-    debugger
+    
     if (moneda1 == "-" || moneda2 == "-") {
         alert("Seleccione el tipo de moneda para realizar la conversión.")
     } else if (monto == "") {
@@ -45,36 +48,52 @@ function conversion() {
         if (moneda1 == "PA" && moneda2 == "PA") {
 
             document.getElementById("resultado").innerHTML = monto
+            resultadoBitcoin = monto * 0.000000090;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PA" && moneda2 == "PC") {
             resultado = monto * 2.67
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000000090;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PA" && moneda2 == "PM") {
             resultado = monto * 0.052
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000000090;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PA" && moneda2 == "D") {
             resultado = monto * 1030
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000000090;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
         }
 
         /* Peso chileno */
         if (moneda1 == "PC" && moneda2 == "PC") {
 
             document.getElementById("resultado").innerHTML = monto
+            resultadoBitcoin = monto * 0.000000034;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PC" && moneda2 == "PA") {
             resultado = monto * 0.37;
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000000034;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PC" && moneda2 == "PM") {
             resultado = monto * 0.019
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000000034;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PC" && moneda2 == "D") {
             resultado = monto * 0.0011
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000000034;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
         }
 
         /* Peso México */
@@ -82,36 +101,52 @@ function conversion() {
         if (moneda1 == "PM" && moneda2 == "PM") {
 
             document.getElementById("resultado").innerHTML = monto
+            resultadoBitcoin = monto * 0.0000017
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PM" && moneda2 == "PA") {
             resultado = monto * 19.28;
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto *0.0000017
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PM" && moneda2 == "PC") {
             resultado = monto * 51.39
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.0000017
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "PM" && moneda2 == "D") {
             resultado = monto * 0.055;
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.0000017;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
         }
 
         /* Dolar */
         if (moneda1 == "D" && moneda2 == "D") {
 
             document.getElementById("resultado").innerHTML = monto
+            resultadoBitcoin = monto * 0.000031;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "D" && moneda2 == "PA") {
             resultado = monto * 1030;
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000031;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "D" && moneda2 == "PC") {
             resultado = monto * 932.98;
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000031;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
 
         } else if (moneda1 == "D" && moneda2 == "PM") {
             resultado = monto * 18.16;
             document.getElementById("resultado").innerHTML = resultado
+            resultadoBitcoin = monto * 0.000031;
+            document.getElementById("bitcoin").innerHTML = resultadoBitcoin;
         }
 
 
